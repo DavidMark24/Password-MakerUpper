6fc9c76a3ca16c9e81b022732ccfe89256f9e98b
David Johnson <david.m.johnson32@gmail.com>
2020-11-22 03:32:47 -0800

    Added if statement and confirmations

--- script.js
+++ script.js
@@ -7,26 +7,49 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword(){
-
+function generatePassword() {
   // Creating Variables
 
-  var userOptions = ["abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
+  var userOptions = [
+    "abcdefghijklmnopqrstuvwxyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "0123456789",
+    " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~",
+  ];
   var userPassword = "";
   var userDisplayed = "";
-  
+
   //Creating prompt for characters
 
-  var charLenght = prompt("How many characters would you like? Pick between 8-128?");
+  var charLenght = prompt(
+    "How many characters would you like? Pick between 8-128."
+  );
 
   // Set the minimun and maximum amount of charcters
 
-  
-
+  if (charLength < 8 || charLength > 128) {
+    for (
+      charLength;
+      charLength < 8 || charLength > 128;
+      charLength = prompt(
+        "Your password must be a number from 8 to 128.")
+      
+    ); 
+      }
 }
+
+// Confirm each characters in use
+
+var lowercase = confirm("Do you want to inlcude lowercase letters?")
+var upperscase = confirm("Do you want to inlcude uppercase letters?")
+var numbers = confirm("Do you want to inlcude numbers?")
+var specials = confirm("Do you want to inlcude special characters?")
+
+var userChoices = [lowercase, upperscase, numbers, specials]
+
+
